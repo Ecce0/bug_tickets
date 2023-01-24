36b2ee0cae2c937c5cf18dfc016b8ba17ea5937a
E. Collier <ecollier871@outlook.com>
2023-01-23 20:41:57 -0500

    backend development, bug: not adding new user to mongoDB compass

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 7000
-const errorHandler = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+
+require('colors')
+require('dotenv').config()
+
+
+//Connection to DB
+connectDB()
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello world')
+  res.status(200).send('Help Desk API')
 })
 
 app.use(express.json())
