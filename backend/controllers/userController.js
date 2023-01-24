3b654813f5b1ccc0b1fda1bd264f1f252fe50346
E. Collier <ecollier871@outlook.com>
2023-01-23 19:30:00 -0500

    backend build, unfinshed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+//@desc Register new user
+//@route /api/users
+//@access Public
+const registerUser = (req, res) => {
+  const { name, email, password } = req.body
+
+  if(!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please include all fields')
+  }
+  res.send('Register Route')
+}
+
+
+//@desc Login user
+//@route /api/users/login
+//@access Public
+const loginUser = (req, res) => {
+  res.send('Login Route')
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Server..')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+const PORT = process.env.PORT || 7000
+const errorHandler = require('./middleware/errorMiddleware')
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world')
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`server listening on ${PORT}`))
