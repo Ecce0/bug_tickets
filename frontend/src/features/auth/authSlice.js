cdf3ff39a349e6428693a6369d24b99b14abb05d
E. Collier <ecollier871@outlook.com>
2023-01-24 23:27:48 -0500

    adding more redux logic

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
-import { createSlice, createAsyncThunk } from '@redux/js/tooklkit'
+import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit'
+import authService from './authService'
 
 const initialState = {
   user: null,
@@ -9,7 +10,13 @@ const initialState = {
 }
 
 export const registerUser = createAsyncThunk('auth/register', async (user, thunkAPI) => {
- console.log(user)
+ try {
+    return await authService.register(user)
+ } catch (error) {
+    const message = (error.response && error.response.data & error.response.data.message) || error.message || error.toString()
+
+    return thunkAPI.rejectWithValue(message)
+ }
 })
 
 export const loginUser = createAsyncThunk('auth/login', async (user, thunkAPI) => {
@@ -21,7 +28,21 @@ export const authSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-
+    builder.
+         addCase(register.pending, (state) => {
+          state.isLoading = true
+         })
+         .addCase(register.fulfilled, (state, action) => {
+          state.isLoading = false
+          state.isSuccess = true
+          state.user = action.payload
+         })
+         .addCase(register.rejected, (state, action) => {
+          state.isLoading = false
+          state.user = null
+          state.isError = true
+          state.message = action.payload
+         })
   }
 })
 
